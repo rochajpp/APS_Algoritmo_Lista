6c5599c654f4aba0119bb2d97559f1c1a5d3272c
João Paulo Rocha <joao.rochap03@gmail.com>
2025-05-08 17:17:01 -0300

    Corrigindo bug de criação de Tasks

--- application.cpp
+++ application.cpp
@@ -1,6 +1,9 @@
 #include<iostream>
 #include<locale.h>
 #include<stdlib.h>
+#include<conio.h>
+#include<cstdlib>
+#include<ctime>
 
 using namespace std;
 
@@ -14,7 +17,8 @@ int quantNode = 0;
 node *firstNode;
 
 void Insert(int pass, char type){
-	node *newNode = new struct node;
+	// Usando new para que a variável seja alaocada no heap
+	node *newNode = new struct node; // Alocando novo nó na memória
 	(*newNode).pass = pass;
 	(*newNode).urgency = type;
 	
@@ -25,6 +29,7 @@ void Insert(int pass, char type){
 		return;
 	}
 	
+
 	node *nodeCheck = firstNode;
 	
 	switch(type){
@@ -37,6 +42,8 @@ void Insert(int pass, char type){
 					firstNode = newNode;
 					break;
 				}
+				
+	
 				
 				// Se a urgência do próximo nó do nó da checagem for média ou baixa ou null ele irá se encaixar após o nó de checagem
 				if((*(*nodeCheck).next).urgency == 'm' || (*(*nodeCheck).next).urgency == 'b' || (*nodeCheck).next == NULL){
@@ -60,7 +67,9 @@ void Insert(int pass, char type){
 					break;
 				}
 				
-				
+					// OBTENDO NÓS EM IFS DE FORMA ERRADA, CORRIGITR
+					cout << (*(*nodeCheck).next).urgency;
+				system("pause");
 				// Se o próximo for baixa ou null ele se encaixa após o nó de checagem
 				if((*(*nodeCheck).next).urgency == 'b' || (*nodeCheck).next == NULL){
 					(*newNode).next = (*nodeCheck).next;
@@ -95,26 +104,176 @@ void Insert(int pass, char type){
 }
 
 
-int main(){
-	Insert(22, 'm');
-	Insert(18, 'a');
-	Insert(10, 'b');
-	Insert(20, 'm');
-	Insert(40, 'm');
-	Insert(80, 'a');
-	Insert(82, 'a');
-	Insert(88, 'a');
-	Insert(92, 'b');
+int menu(char title [100], int size, char options[10][100]){
+	int ch;
+	int select = 0;
 	
-	node *nodeView = firstNode;
-	
-	for(int i = 0; i < quantNode; i++){
-		cout << "|Senha: " << (*nodeView).pass << " " << "Tipo: " << (*nodeView).urgency << "| --> ";
-		nodeView = (*nodeView).next;
+	while(true){
+		cout << "========== " << title << "==========" << endl;
+		for(int i = 0; i < size; i++){
+			if(select == i){
+				printf("> %s\n", options[i]);
+				continue;
+			}
+			printf("%s\n", options[i]);
+		}
+		cout << "=============================================";
+		
+		ch = _getch(); // Pegar dados de teclado
+
+		switch(ch){
+			case 27:
+				// Tecla ESC
+				return -1;
+				break;
+
+			case 224: //Prefixo de tecla de setas
+				ch = _getch();
+
+				switch(ch){
+					case 72: // Seta pra cima
+						if(select > 0){
+							select -= 1;
+						} 
+						break;
+
+					case 80: // Seta pra baixo
+						if(select < size - 1){
+							select += 1;
+						}
+						break;
+				}
+			break;
+			
+
+			case 13: // Enter
+				return select;
+				break;
+		}
+		
+		// Sempre que uma tecla é pressionada ele limpa a tela dando a impressão de que estamos movimentando seta
+		system("cls");
 	}
 	
-	cout << "NULL" << endl;
-	
-	system("pause");
 }
+
+
+bool ExistPass(int pass){
+	node *checkNode = firstNode;
+	for(int i = 0; i < quantNode; i++){
+		if((*checkNode).pass == pass){
+			return true;
+		}
+		
+		checkNode = (*checkNode).next;
+	}
+	
+	return false;
+}
+
+
+void app(){
+	char options[10][100] = {"Abrir Chamado", "Listar chamados", "Sair"}, optionsUrgency[10][100] = {"Alta", "Média", "Baixa", "Voltar"}; // Opções de menus
+	int select, selectUrgency; // Selecionadores de opções
+	node *nodeView; // Ponteiro para referencia de listagem
+	
+	// Inicializando srand
+	srand(time(NULL));	
+	
+	while(true){
+	
+		select = menu("Selecione a opção desejada", 3, options);
+		
+		switch(select){
+			case 0:
+				system("cls");
+				
+				selectUrgency = menu("Selecione a urgência do chamado", 4, optionsUrgency);
+				
+				int key;
+				
+				do{
+					key = rand() % 1000 + 1;
+				} while(ExistPass(key));
+				
+				switch(selectUrgency){
+					case 0:
+						Insert(key, 'a');
+						break;
+					case 1:
+						Insert(key, 'm');
+						break;
+					case 2:
+						Insert(key, 'b');
+						break;
+					case 3:
+						system("cls");
+						continue;
+						break;
+				}
+				
+				system("cls");
+				cout << "Chamado com a senha " << key << " aberto com sucesso!" << endl;
+				system("pause");
+				system("cls");
+				continue;
+				break;
+				
+			case 1:
+				system("cls");
+				if(quantNode <= 0){
+					cout << "Nenhum chamado cadastrado" << endl;
+					system("pause");
+					system("cls");
+					continue;
+					break;
+				}
+				
+				nodeView = firstNode;
+				for(int i = 0; i < quantNode; i++){
+					cout << "=======================" << endl;
+					
+					if((*nodeView).urgency == 'a'){
+						cout << "|Senha: " << (*nodeView).pass  << endl << "|Urgência: Alta" << endl;
+					}
+					
+					if((*nodeView).urgency == 'm'){
+						cout << "|Senha: " << (*nodeView).pass  << endl << "|Urgência: Média" << endl;
+					}
+					
+					if((*nodeView).urgency == 'b'){
+						cout << "|Senha: " << (*nodeView).pass  << endl << "|Urgência: Baixa" << endl;
+					}
+					
+					cout << "=======================" << endl;
+					cout << "          |" << endl;
+					cout << "          |" << endl;
+					cout << "          V" << endl;
+			
+					nodeView = (*nodeView).next;
+				}
+				
+				cout << "         NULL" << endl;
+				
+				system("pause");
+				system("cls");
+				
+				continue;
+				
+				break;
+				
+			case 3:
+				break;
+		}
+		break;
+	}
+}
+
+
+int main(){
+	setlocale(LC_ALL, "Portuguese");
+	
+	app();
+}
+
 
